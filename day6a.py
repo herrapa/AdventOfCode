e22bc5020e978b029d918bbfd50ba859e4d2277c
Joakim Andersson <joakimsson@gmail.com>
2021-12-07 07:38:41 +0100

    Add day6a solution

---
+++ day6a.py
@@ -0,0 +1,13 @@
+with open("inputs/input6.txt", encoding="utf8") as infile:
+    inp = [int(x) for x in infile.readline().strip().split(",")]
+    toAppend = []
+    for n in range(256):
+        for i in range(len(inp)):
+            if inp[i] == 0:
+                toAppend.append(8)
+                inp[i] = 6
+            else:
+                inp[i] = (inp[i] - 1)
+        inp += toAppend
+        toAppend = []
+    print(len(inp))
